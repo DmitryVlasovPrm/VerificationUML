e117b244ca64a3f9bf384ec35fd15e59fa3c7e11
Dmitry Vlasov <dima.vlasov@icloud.com>
2021-06-02 15:29:41 +0500

    Починил отрисовку ошибок для диаграмм классов

--- Verification/Main.Designer.cs
+++ Verification/Main.Designer.cs
@@ -75,7 +75,7 @@ namespace Verification
             this.menuRate});
 			this.menu.Location = new System.Drawing.Point(0, 0);
 			this.menu.Name = "menu";
-			this.menu.Size = new System.Drawing.Size(1199, 31);
+			this.menu.Size = new System.Drawing.Size(1199, 27);
 			this.menu.TabIndex = 0;
 			this.menu.Text = "menu";
 			// 
@@ -85,41 +85,41 @@ namespace Verification
             this.выбратьToolStripMenuItem,
             this.сохранитьToolStripMenuItem});
 			this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-			this.файлToolStripMenuItem.Size = new System.Drawing.Size(64, 27);
+			this.файлToolStripMenuItem.Size = new System.Drawing.Size(53, 23);
 			this.файлToolStripMenuItem.Text = "Файл";
 			// 
 			// выбратьToolStripMenuItem
 			// 
 			this.выбратьToolStripMenuItem.Name = "выбратьToolStripMenuItem";
-			this.выбратьToolStripMenuItem.Size = new System.Drawing.Size(259, 28);
+			this.выбратьToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
 			this.выбратьToolStripMenuItem.Text = "Добавить файл(ы)";
 			this.выбратьToolStripMenuItem.Click += new System.EventHandler(this.выбратьToolStripMenuItem_Click);
 			// 
 			// сохранитьToolStripMenuItem
 			// 
 			this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
-			this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(259, 28);
+			this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
 			this.сохранитьToolStripMenuItem.Text = "Сохранить результат";
 			this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
 			// 
 			// справкаToolStripMenuItem
 			// 
 			this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
-			this.справкаToolStripMenuItem.Size = new System.Drawing.Size(90, 27);
+			this.справкаToolStripMenuItem.Size = new System.Drawing.Size(74, 23);
 			this.справкаToolStripMenuItem.Text = "Справка";
 			this.справкаToolStripMenuItem.Click += new System.EventHandler(this.справкаToolStripMenuItem_Click);
 			// 
 			// menuSettings
 			// 
 			this.menuSettings.Name = "menuSettings";
-			this.menuSettings.Size = new System.Drawing.Size(108, 27);
+			this.menuSettings.Size = new System.Drawing.Size(89, 23);
 			this.menuSettings.Text = "Настройки";
 			this.menuSettings.Click += new System.EventHandler(this.menuSettings_Click);
 			// 
 			// menuRate
 			// 
 			this.menuRate.Name = "menuRate";
-			this.menuRate.Size = new System.Drawing.Size(92, 27);
+			this.menuRate.Size = new System.Drawing.Size(76, 23);
 			this.menuRate.Text = "Оценить";
 			this.menuRate.Click += new System.EventHandler(this.menuRate_Click);
 			// 
@@ -130,13 +130,13 @@ namespace Verification
 			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
 			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 1);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 31);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 27);
 			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
 			this.tableLayoutPanel1.RowCount = 2;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.03771F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.962288F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(1199, 708);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(1199, 712);
 			this.tableLayoutPanel1.TabIndex = 1;
 			// 
 			// tableLayoutPanel2
@@ -153,7 +153,7 @@ namespace Verification
 			this.tableLayoutPanel2.RowCount = 1;
 			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
 			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel2.Size = new System.Drawing.Size(1199, 637);
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(1199, 641);
 			this.tableLayoutPanel2.TabIndex = 0;
 			// 
 			// diagramPicture
@@ -163,7 +163,7 @@ namespace Verification
 			this.diagramPicture.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.diagramPicture.Location = new System.Drawing.Point(3, 3);
 			this.diagramPicture.Name = "diagramPicture";
-			this.diagramPicture.Size = new System.Drawing.Size(788, 631);
+			this.diagramPicture.Size = new System.Drawing.Size(788, 635);
 			this.diagramPicture.TabIndex = 0;
 			this.diagramPicture.TabStop = false;
 			// 
@@ -182,17 +182,17 @@ namespace Verification
 			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45F));
 			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45F));
 			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-			this.tableLayoutPanel4.Size = new System.Drawing.Size(405, 637);
+			this.tableLayoutPanel4.Size = new System.Drawing.Size(405, 641);
 			this.tableLayoutPanel4.TabIndex = 1;
 			// 
 			// groupBox2
 			// 
 			this.groupBox2.Controls.Add(this.diagramsGV);
 			this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.groupBox2.Location = new System.Drawing.Point(3, 289);
+			this.groupBox2.Location = new System.Drawing.Point(3, 291);
 			this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
 			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(399, 283);
+			this.groupBox2.Size = new System.Drawing.Size(399, 285);
 			this.groupBox2.TabIndex = 1;
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "Диаграммы";
@@ -205,7 +205,7 @@ namespace Verification
 			this.diagramsGV.BackgroundColor = System.Drawing.Color.White;
 			this.diagramsGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.diagramsGV.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.diagramsGV.Location = new System.Drawing.Point(3, 23);
+			this.diagramsGV.Location = new System.Drawing.Point(3, 19);
 			this.diagramsGV.MultiSelect = false;
 			this.diagramsGV.Name = "diagramsGV";
 			this.diagramsGV.ReadOnly = true;
@@ -213,7 +213,7 @@ namespace Verification
 			this.diagramsGV.RowHeadersWidth = 51;
 			this.diagramsGV.RowTemplate.Height = 24;
 			this.diagramsGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-			this.diagramsGV.Size = new System.Drawing.Size(393, 257);
+			this.diagramsGV.Size = new System.Drawing.Size(393, 263);
 			this.diagramsGV.TabIndex = 0;
 			this.diagramsGV.SelectionChanged += new System.EventHandler(this.diagramsGV_SelectionChanged);
 			// 
@@ -224,7 +224,7 @@ namespace Verification
 			this.errorsGB.Location = new System.Drawing.Point(3, 0);
 			this.errorsGB.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
 			this.errorsGB.Name = "errorsGB";
-			this.errorsGB.Size = new System.Drawing.Size(399, 283);
+			this.errorsGB.Size = new System.Drawing.Size(399, 285);
 			this.errorsGB.TabIndex = 0;
 			this.errorsGB.TabStop = false;
 			this.errorsGB.Text = "Ошибки";
@@ -237,7 +237,7 @@ namespace Verification
 			this.errorsGV.BackgroundColor = System.Drawing.Color.White;
 			this.errorsGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.errorsGV.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.errorsGV.Location = new System.Drawing.Point(3, 23);
+			this.errorsGV.Location = new System.Drawing.Point(3, 19);
 			this.errorsGV.MultiSelect = false;
 			this.errorsGV.Name = "errorsGV";
 			this.errorsGV.ReadOnly = true;
@@ -245,7 +245,7 @@ namespace Verification
 			this.errorsGV.RowHeadersWidth = 51;
 			this.errorsGV.RowTemplate.Height = 24;
 			this.errorsGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-			this.errorsGV.Size = new System.Drawing.Size(393, 257);
+			this.errorsGV.Size = new System.Drawing.Size(393, 263);
 			this.errorsGV.TabIndex = 0;
 			this.errorsGV.SelectionChanged += new System.EventHandler(this.errorsGV_SelectionChanged);
 			// 
@@ -259,7 +259,7 @@ namespace Verification
 			this.tableLayoutPanel5.Controls.Add(this.btDelete, 1, 0);
 			this.tableLayoutPanel5.Controls.Add(this.btAdd, 2, 0);
 			this.tableLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tableLayoutPanel5.Location = new System.Drawing.Point(0, 572);
+			this.tableLayoutPanel5.Location = new System.Drawing.Point(0, 576);
 			this.tableLayoutPanel5.Margin = new System.Windows.Forms.Padding(0);
 			this.tableLayoutPanel5.Name = "tableLayoutPanel5";
 			this.tableLayoutPanel5.RowCount = 1;
@@ -308,11 +308,11 @@ namespace Verification
 			// tableLayoutPanel3
 			// 
 			this.tableLayoutPanel3.ColumnCount = 2;
-			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 76.64721F));
-			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.35279F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.72727F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.27273F));
 			this.tableLayoutPanel3.Controls.Add(this.btVerify, 2, 0);
 			this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 637);
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 641);
 			this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
 			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
 			this.tableLayoutPanel3.RowCount = 1;
@@ -324,10 +324,10 @@ namespace Verification
 			// 
 			this.btVerify.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.btVerify.Enabled = false;
-			this.btVerify.Location = new System.Drawing.Point(926, 12);
+			this.btVerify.Location = new System.Drawing.Point(878, 12);
 			this.btVerify.Margin = new System.Windows.Forms.Padding(7, 12, 6, 12);
 			this.btVerify.Name = "btVerify";
-			this.btVerify.Size = new System.Drawing.Size(267, 47);
+			this.btVerify.Size = new System.Drawing.Size(315, 47);
 			this.btVerify.TabIndex = 1;
 			this.btVerify.Text = "Верифицировать";
 			this.btVerify.UseVisualStyleBackColor = true;
@@ -335,7 +335,7 @@ namespace Verification
 			// 
 			// Main
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1199, 739);
 			this.Controls.Add(this.tableLayoutPanel1);
--- Verification/MinCoordinates.cs
+++ Verification/MinCoordinates.cs
@@ -8,19 +8,21 @@ namespace Verification
         public static (int, int) Compute(Image<Bgra, byte> image)
         {
             var grayImage = image.Convert<Gray, byte>();
+            var firstPixel = grayImage.Data[0, 0, 0];
+            var threshold = firstPixel == 0 ? 0 : 255;
             var imageHeight = grayImage.Height;
             var imageWidth = grayImage.Width;
 
             var minX = imageWidth;
             for (var i = 0; i < imageHeight; i++)
                 for (var j = 0; j < imageWidth / 4; j++)
-                    if (grayImage.Data[i, j, 0] != 0 && j < minX)
+                    if (grayImage.Data[i, j, 0] != threshold && j < minX)
                         minX = j;
 
             var minY = imageHeight;
             for (var i = 0; i < imageWidth; i++)
                 for (var j = 0; j < imageHeight / 4; j++)
-                    if (grayImage.Data[j, i, 0] != 0 && j < minY)
+                    if (grayImage.Data[j, i, 0] != threshold && j < minY)
                         minY = j;
 
             return (minX, minY);
